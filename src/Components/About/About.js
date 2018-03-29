8020f980904602b0a5e9f6591a7fdc81dbc93796
Garrett <Garrettcrg@gmail.com>
2018-03-29 16:52:15 -0500

    changed-wording

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -25,23 +25,24 @@ class About extends Component {
           and thats when my experience with tech started to grow
         </p>
         <p>
-          when i was 9 i remember using the command line on windows xp to send a
-          message from my computer up stairs to the computer down stairs
-        </p>
-        <p>
-          through out ive owned several apple products (ipodtouches, ipads ,
-          iphone's and more) and used to (jailbreak) them where i installed 3rd
-          party packages and application on to the devices through the root and
+          when i was a kid i remember using the command line on windows xp to
+          send a message from my computer up stairs to the computer down stairs
+          i also owned several apple products (ipodtouches, ipads, iphone's and
+          more) and used to (jailbreak) them where i installed 3rd party
+          packages and application on to the devices through the root and
           package manager or through ssh
         </p>
-        <p>when i was 11 i dual booted my pc with linux and windows </p>
         <p>
-          when i was 12 i made my pc a (Hackintosh) where i installed the mac
-          operating system on windows hardware
+          when i was a teenager i dual booted my pc with linux and windows and
+          also i made my pc a (Hackintosh) where i installed the mac operating
+          system on windows hardware then when i was 16 i built my own pc i
+          picked out all the components and put it all together that was an
+          amazing learning experience
         </p>
         <p>
-          then when i was 16 i built my own pc i picked out all the components
-          and put it all together that was an amazing learning experience
+          and fast forward to current time i am a full-stack web developer i
+          know technologys like javascript css html node.js react.js sql github
+          and i hope that my learning doesent stop{" "}
         </p>
       </div>
     );
