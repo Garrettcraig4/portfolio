10967eaee593dad4dc6deffb43e40a0dac20f38d
Garrett <Garrettcrg@gmail.com>
2018-04-04 21:00:23 -0500

    added-download-for-resume-and-link-email

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Button } from "reactstrap";
-
+import "./About.css";
 class About extends Component {
   render() {
     return (
@@ -9,6 +9,7 @@ class About extends Component {
 
         <h3>Education</h3>
         <p> DevMountain immersive web development bootcamp </p>
+        <p>2018-2018</p>
         <p>
           DevMountain is a 13 week bootcamp where the teach all necessary skills
           and practices required to start a career as a web developer{" "}
@@ -21,28 +22,30 @@ class About extends Component {
         </Button>
         <h3>My Experience with tech </h3>
         <p>
-          when i was 3 my dad came home from work and built me my first computer
-          and thats when my experience with tech started to grow
+          when I was 3 my dad came home from work and built me my first computer
+          and thats when my experience with tech started to grow.
         </p>
         <p>
-          when i was a kid i remember using the command line on windows xp to
-          send a message from my computer up stairs to the computer down stairs
-          i also owned several apple products (ipodtouches, ipads, iphone's and
+          when I was a kid I remember using the command line on windows xp to
+          send a message from my computer up stairs to the computer down stairs{" "}
+          <br />
+          I also owned several apple products (ipodtouches, ipads, iphone's and
           more) and used to (jailbreak) them where i installed 3rd party
           packages and application on to the devices through the root and
           package manager or through ssh
         </p>
         <p>
-          when i was a teenager i dual booted my pc with linux and windows and
-          also i made my pc a (Hackintosh) where i installed the mac operating
-          system on windows hardware then when i was 16 i built my own pc i
-          picked out all the components and put it all together that was an
-          amazing learning experience
+          when I was a teen i dual booted my pc with linux and windows and also
+          I made my pc a (Hackintosh) where I installed the mac operating system
+          on windows hardware then when I was 16 I built my own pc I picked out
+          all the components and put it all together that was an amazing
+          learning experience
         </p>
         <p>
-          and fast forward to current time i am a full-stack web developer i
-          know technologys like javascript css html node.js react.js sql github
-          and i hope that my learning doesent stop{" "}
+          and fast forward to current time I am a full-stack web developer I
+          have learned technologies like javascript, css, html, node.js,
+          react.js, sql and github.I hope that my learning doesn't stop and I
+          keep learning new technologies
         </p>
       </div>
     );
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
-
+import GResume from "../../assets/GResume.pdf";
 export default class Example extends React.Component {
   render() {
     return (
       <div>
         <h1 className="display-3">Contact Me</h1>
         <ListGroup>
-          <ListGroupItem tag="a" href="#" action>
+          <ListGroupItem tag="a" href="mailto:Garrettcrg@gmail.com" action>
             Email: Garrettcrg@gmail.com
           </ListGroupItem>
           <ListGroupItem
@@ -20,8 +20,14 @@ export default class Example extends React.Component {
           <ListGroupItem tag="a" href="https://github.com/Garrettcraig4" action>
             Github
           </ListGroupItem>
-          <ListGroupItem tag="a" href="" action>
-            Resume
+          <ListGroupItem
+            tag="a"
+            action
+            href={GResume}
+            download="Garrett Craig Resume.pdf"
+            target="_blank"
+          >
+            Download Resume
           </ListGroupItem>
         </ListGroup>
         <p />
